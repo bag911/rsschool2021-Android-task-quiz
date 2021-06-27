44953c25cbf97979ef8a16d442548169168ba155
bag911 <bagish3000@gmail.com>
2021-06-28 01:07:30 +0300

    fix: modified theme and navigation

--- app/src/main/java/com/rsschool/quiz/MainActivity.kt
+++ app/src/main/java/com/rsschool/quiz/MainActivity.kt
@@ -3,9 +3,12 @@ package com.rsschool.quiz
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
+import android.widget.Toolbar
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.rsschool.quiz.databinding.ActivityMainBinding
 import com.rsschool.quiz.quizfragments.FragmentNavigation
@@ -14,66 +17,56 @@ import com.rsschool.quiz.quizfragments.FragmentNavigation
 class MainActivity : AppCompatActivity(), FragmentNavigation {
 
     private lateinit var binding: ActivityMainBinding
-
+    private lateinit var navController: NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(LayoutInflater.from(this))
         setContentView(binding.root)
-//
+
         val host =
             supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
-        val navController = host.navController
+        navController = host.navController
         val appBarConfiguration = AppBarConfiguration(navController.graph)
-        binding.toolbar.setupWithNavController(navController, appBarConfiguration)
 
+        setSupportActionBar(binding.toolbar)
+        setupActionBarWithNavController(navController, appBarConfiguration)
+
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 
     fun openFirstFragment() {
         window.statusBarColor = resources.getColor(R.color.title_text)
         setTheme(R.style.Theme_Quiz_First)
-        binding.appBarLayout.setBackgroundColor(resources.getColor(R.color.secondary_text))
-        binding.toolbarTitle.text = getString(R.string.question_1)
-        binding.appBarLayout.visibility = View.VISIBLE
-
     }
 
     fun openSecondFragment() {
         window.statusBarColor = resources.getColor(R.color.title_text_second)
         setTheme(R.style.Theme_Quiz_Second)
-        binding.appBarLayout.setBackgroundColor(resources.getColor(R.color.secondary_text_second))
-        binding.toolbarTitle.text = getString(R.string.question_2)
-        binding.appBarLayout.visibility = View.VISIBLE
 
     }
 
     fun openThirdFragment() {
         window.statusBarColor = resources.getColor(R.color.title_text_third)
         setTheme(R.style.Theme_Quiz_Third)
-        binding.appBarLayout.setBackgroundColor(resources.getColor(R.color.secondary_text_third))
-        binding.toolbarTitle.text = getString(R.string.question_3)
-        binding.appBarLayout.visibility = View.VISIBLE
-
     }
 
     fun openFourthFragment() {
         window.statusBarColor = resources.getColor(R.color.title_text_fourth)
         setTheme(R.style.Theme_Quiz_Fourth)
-        binding.appBarLayout.setBackgroundColor(resources.getColor(R.color.secondary_text_fourth))
-        binding.toolbarTitle.text = getString(R.string.question_4)
-        binding.appBarLayout.visibility = View.VISIBLE
 
     }
 
     fun openFifthFragment() {
         window.statusBarColor = resources.getColor(R.color.title_text_fifth)
         setTheme(R.style.Theme_Quiz_Fifth)
-        binding.appBarLayout.setBackgroundColor(resources.getColor(R.color.secondary_text_fifth))
-        binding.toolbarTitle.text = getString(R.string.question_5)
-        binding.appBarLayout.visibility = View.VISIBLE
     }
 
     private fun openResultFragment() {
-        binding.appBarLayout.visibility = View.GONE
+        binding.toolbar.visibility = View.GONE
 
     }
 
